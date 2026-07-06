26a869b36ba9f4a078dc5edc9cdef8a484528a9a
Rodrigo Lapsky <rodrigolapsky@gmail.com>
2026-07-06 09:59:10 -0300

    Create or replace tb_geolocation table with cleaned data
    This query creates or replaces the geolocation table with cleaned data, ensuring no rows with all NULL values are included.

--- sql_bigquery/silver/tb_geolocation.sql
+++ sql_bigquery/silver/tb_geolocation.sql
@@ -1,1 +1,14 @@
-
+CREATE OR REPLACE TABLE `study-workspace-500319.project_ecommerce_silver.tb_geolocation` AS
+SELECT
+    geolocation_zip_code_prefix,
+    geolocation_lat,
+    geolocation_lng,
+    NULLIF(TRIM(geolocation_city), '') AS geolocation_city,
+    NULLIF(TRIM(geolocation_state), '') AS geolocation_state
+FROM `study-workspace-500319.project_ecommerce_silver.tb_geolocation`
+WHERE NOT (
+    geolocation_zip_code_prefix IS NULL
+    AND geolocation_lat IS NULL
+    AND geolocation_lng IS NULL
+    AND geolocation_city IS NULL
+    AND geolocation_state IS NULL)
